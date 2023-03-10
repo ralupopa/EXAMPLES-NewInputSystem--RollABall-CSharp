a89cae4036325e6e9a736e9063bcbbea57cd318c
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2023-03-10 20:46:31 +0200

    WIP Test for gameplay to WIN

--- RollABall/Assets/Editor/Tests_AltTester/Tests_RollABall.cs
+++ RollABall/Assets/Editor/Tests_AltTester/Tests_RollABall.cs
@@ -21,7 +21,7 @@ public class Tests_RollABall
     }
 
     [Test]
-    public void TestMoveBallToCollectATLeast5Boxes()
+    public void TestMoveBallUsingPressKeysToCollectAtLeast5Boxes()
     {
         altDriver.LoadScene("MiniGame");
         var ball = altDriver.FindObject(By.NAME, "Player");
@@ -78,5 +78,87 @@ public class Tests_RollABall
         Debug.Log("Ball moved LEFT + DOWN");
     }
 
+    [Test]
+    public void TestMoveBallUsingMoveMouseToCollectAtLeast5Boxes()
+    
+    {
+        altDriver.LoadScene("MiniGame");
+        var ball = altDriver.FindObject(By.NAME, "Player");
+        var initialPosition = ball.GetWorldPosition();
+
+        altDriver.MoveMouse(new AltVector2(120, 0), 1.5f);
+        altDriver.MoveMouse(new AltVector2(0, 60), 1f);
+
+        altDriver.MoveMouse(new AltVector2(-30, 40), 1.5f);
+
+        var countPicked = altDriver.FindObject(By.NAME, "CountText");
+        Debug.Log("Collected "+ countPicked.GetText() );
+        Thread.Sleep(2000);
+
+        altDriver.MoveMouse(new AltVector2(-30, -45), 3f);
+        Thread.Sleep(2000);
+
+        countPicked = altDriver.FindObject(By.NAME, "CountText");
+        Debug.Log("Collected "+ countPicked.GetText() );
+
+        altDriver.MoveMouse(new AltVector2(-50, 40), 3f);
+        Thread.Sleep(2000);
+        altDriver.MoveMouse(new AltVector2(40, 20), 3f);
+        Thread.Sleep(2000);
+        altDriver.MoveMouse(new AltVector2(100, 100), 3f);
+        Thread.Sleep(2000);
+        altDriver.MoveMouse(new AltVector2(50, 0), 3f);
+        Thread.Sleep(2000);
+        Assert.GreaterOrEqual(countPicked.GetText(), "Count: 5");
+
+        ball = altDriver.FindObject(By.NAME, "Player");
+        var finalPosition = ball.GetWorldPosition();
+        Assert.AreNotEqual(initialPosition.x, finalPosition.x);
+    }
+
+    [Test]
+    public void TestMoveBallMoveMouseAndMathToCollectAtLeast5Boxes()
+    
+    {
+        altDriver.LoadScene("MiniGame");
+        var ball = altDriver.FindObject(By.NAME, "Player");
+        var initialPosition = ball.GetWorldPosition();
+
+        altDriver.MoveMouse(new AltVector2(-120, 0), 1.3f);
+
+        ball = altDriver.FindObject(By.NAME, "Player");
+        var leftPosition = ball.GetWorldPosition();
+        
+        Thread.Sleep(2000);
+        var x = leftPosition.x;
+        var y = leftPosition.y;
+
+        var radius = 7;
+        Debug.Log("Radius is "+ radius );
+        var circumference = 2 * Mathf.PI * radius;
+        Debug.Log("Circumference is "+ circumference );
+
+        altDriver.MoveMouse(new AltVector2(50, Mathf.Sin(x)), 1.5f);
+        Thread.Sleep(2000);
+        var countPicked = altDriver.FindObject(By.NAME, "CountText");
+        Debug.Log("Collected "+ countPicked.GetText() );
+        Assert.AreEqual(countPicked.GetText(), "Count: 2");
+
+        altDriver.MoveMouse(new AltVector2(2 * radius, circumference), 1.5f, true);
+        Thread.Sleep(2000);
+        countPicked = altDriver.FindObject(By.NAME, "CountText");
+        Assert.GreaterOrEqual(countPicked.GetText(), "Count: 4");
+
+        Thread.Sleep(2000);
+        altDriver.MoveMouse(new AltVector2(-2 * radius, -circumference), 1.5f);
+        Thread.Sleep(2000);
+        countPicked = altDriver.FindObject(By.NAME, "CountText");
+        Assert.GreaterOrEqual(countPicked.GetText(), "Count: 5");
+
+        ball = altDriver.FindObject(By.NAME, "Player");
+        var finalPosition = ball.GetWorldPosition();
+        Assert.AreNotEqual(initialPosition.x, finalPosition.x);
+
+    }
 
 }
